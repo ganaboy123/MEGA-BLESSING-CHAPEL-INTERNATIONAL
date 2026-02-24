0d5cb5f2924b699c99ba1851cb93e5496d1215e5
ganaboy123 <harrisonappiah088@Gmail.com>
2026-02-24 19:41:28 +0000

    Fix static file serving order

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ const submissions = {
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files first
+app.use(express.static(path.join(__dirname)));
+
+// Root route serves gateway.html
 app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "gateway.html"));
 });
-
-app.use(express.static(path.join(__dirname)));
 
 const isValidEmail = (email) => {
   if (typeof email !== "string") {
